9a870b73dc01fbb33932827a30523f7f832d50ef
melancholive <melancholive@users.noreply.github.com>
2026-09-24 17:05:59 -0400

    added seed url fetching + robot.txt check + priority scoring

--- crawler.py
+++ crawler.py
@@ -1,35 +1,194 @@
 import urllib.request
+from urllib.robotparser import RobotFileParser
+from urllib.parse import urljoin, urlparse, parse_qs
+import tldextract
+import base64
+
 from bs4 import BeautifulSoup
+from queue import PriorityQueue
 
-url = "https://google.com/search?q=xiaohongshu"
+import random
+import math
+import threading
+import time
+from datetime import datetime
 
-# log to output files
-def log(url, data):
-    with open("log.txt", "w", encoding="utf-8") as file:
-        file.write(url)
-        file.write('\n')
+# header to spoof user-agent value
+headers = {
+    "User-Agent": f"testing-a-web-crawler-aowiebfvoawieka",
+    "Accept-Language": "en-US,en;q=0.9",
+}
 
-    with open("webpages/webpage.html", "w", encoding="utf-8") as file:
-        soup = BeautifulSoup(data)
-        file.write(soup.prettify())
+# logging data
+start_time = datetime.now()
+num_errors = {}
 
-# html parsing
-from html.parser import HTMLParser
-class URLParser(HTMLParser):
-    def handle_starttag(self, tag, attrs):
-        print("Encountered a start tag:", tag)
+# limit num of sites visted
+num_visited = 0
+max_download = 30
+visit_delay = 1.0
 
-    def handle_data(self, data):
-        if data:
-            print("Encountered some data  :", data[:100])
+site_queue = PriorityQueue()
+visited_sites = {}
+visited_urls = set()
 
-response = urllib.request.urlopen(url)
-content = response.read()
-parser = URLParser()
-# parse html content
-parser.feed(content[:1000].decode("utf-8", errors="ignore")) # convert bytes into string
-# log site visit
-log(url, (content[:100].decode("utf-8", errors="ignore")))
+def parse_url(url):
+    parsed_url = tldextract.extract(url)
+    return {
+        # Reference URL:
+        # https://shop.example.co.uk:443/products/phones?category=smartphones#reviews
+
+        # "sub": parsed_url.subdomain, # "shop"
+        # "domain": parsed_url.domain, # "example"
+        # "suffix": parsed_url.suffix, # "co.uk"
+        "superdomain": parsed_url.top_domain_under_public_suffix, # "example.co.uk"
+        "fqdn" : parsed_url.fqdn, # "shop.example.co.uk"
+        "scheme" : urlparse(url).scheme, # https
+        "netlock": urlparse(url).netloc, # "shop.example.co.uk:443"
+        "path": urlparse(url).path, # "/products/phones"
+    }
 
 #  Robot Exclusion Protocol
 # https://yoast.com/ultimate-guide-robots-txt/
+
+# keep track of each robots.txt for each domain
+robots_cache = {}   
+
+def robot_fetch(url, parsed, user_agent="*"):
+    # check if website can be scraped
+    robots_url = f"{parsed['scheme']}://{parsed['netlock']}/robots.txt"
+
+    if robots_url not in robots_cache:
+        rp = RobotFileParser()
+        rp.set_url(robots_url)
+
+        try:
+            rp.read()
+        except Exception:
+            rp.parse([])  # empty rules, allow everything
+
+        robots_cache[robots_url] = rp
+
+    return robots_cache[robots_url].can_fetch("*", url)
+
+
+def fetch_page(url):
+    request = urllib.request.Request(url, headers=headers)
+    response = urllib.request.urlopen(request, timeout=5)
+
+    final_url = response.geturl() # get the final url after redirects
+
+    content = response.read().decode("utf-8", errors="ignore") # convert from bytes to text
+    soup = BeautifulSoup(content, "html.parser")
+
+    return final_url, soup
+
+# FETCH SEED URLS FROM USER INPUT
+search_term = input("Enter your search term: ")
+search_term = search_term.replace(" ", "+") # accounts for terms with spaces
+seed_url, initial_soup = fetch_page(f"https://bing.com/?q={search_term}")
+
+seed_urls = []
+search_results = initial_soup.select("h2 a") # search results are nested here
+
+for result in search_results[:10]: 
+    result = result.get('href') # get the link portion
+    marker = "&u=a1" # start of the hashed url
+
+    i = result.find(marker)
+    if i != -1: # make sure that the marker exists
+        # splice the hashed portion of the redirect url
+        u = result[i + len(marker):].split("&", 1)[0]
+
+        # add buffer to make sure right size for hash
+        u += "=" * (-len(u) % 4) 
+
+        # convert hash to usable url
+        final_url = base64.urlsafe_b64decode(u).decode("utf-8", "ignore")
+
+        print(final_url)
+
+        site_queue.put((-2, 0, final_url, parse_url(final_url))) # priority, depth, url
+        visited_urls.add(final_url)
+
+def site_priority(p, priority_score = -2):
+    # priority queue uses min-heap --> start at a negative number
+    # add penalty as you revisit superdomain and fqdn
+
+    superdomain_count = visited_sites.get(p['superdomain'], 0)
+    priority_score += math.log1p(superdomain_count)
+
+    fqdn_count = visited_sites.get(p['fqdn'],0)
+    
+    priority_score += math.log1p(fqdn_count)
+    return priority_score
+
+# log to log.txt and /webpages directory
+def log(url, p, soup, data, status_code, priority, depth):
+    with open("log.txt", "a", encoding="utf-8") as file:
+        file.write(f"{datetime.now()} | {depth} | {len(data)} bytes | {status_code} | page priority : {priority} | {p['superdomain']} | {p['fqdn']} | {url}\n")
+        
+
+    with open(f"webpages/webpage{num_visited}.html", "w", encoding="utf-8") as file:
+        file.write(soup.prettify())
+
+while not site_queue.empty() and num_visited < max_download:
+    priority, depth, url, p = site_queue.get()
+    while site_priority(p) != priority:
+        site_queue.put((site_priority(p), depth, url, p))
+        priority, depth, url, p = site_queue.get()
+
+    try:
+        if not robot_fetch(url,p):
+            print(f"ROBOT.TXT: prohibited at {url}")
+            continue
+        
+        request = urllib.request.Request(url, headers=headers)
+        response = urllib.request.urlopen(request, timeout=5)
+        status_code = response.status
+        content_type = response.headers.get("Content-Type", "")
+
+        if "text/html" not in content_type.lower():
+            print(f"NOT HTML: {content_type!r} at {url}")
+            continue
+        
+        content = response.read().decode("utf-8", errors="ignore")  # convert bytes into string
+        soup = BeautifulSoup(content, "html.parser")
+
+        # log site visit        
+        num_visited += 1
+        
+        visited_sites[p["superdomain"]]  = visited_sites.get(p["superdomain"], 0) + 1
+        if p["fqdn"] != p["superdomain"]: # prevent double counting if they are the same
+            visited_sites[p["fqdn"]] = visited_sites.get(p["fqdn"], 0) + 1
+
+        log(url, p, soup, content, status_code, site_priority(p), depth)
+        
+        # find links on site
+        for a in soup.find_all("a", href=True):
+            # if using base tag, append url to the domain
+            link = urljoin(url, a["href"])
+
+            if link not in visited_urls and link.find("cgi") == -1:
+                visited_urls.add(link)
+                p = parse_url(link) 
+                site_queue.put((site_priority(p), depth + 1, link, p))
+
+
+    except Exception as e:
+        # 404 : the web server reached out successfully
+        # but it could not find the specific page or file you requested
+
+        # 403 : FORBIDDEN the web server understands your request
+        # but refuses to grant access to the page or file
+
+        # 302 : infinite loop from redirects
+
+        # <url oppen error [ssl: certificate_verify_failed] certificate verify failed
+        
+        num_errors[e] = num_errors.get(e, 0) + 1
+        print(f"ERROR: {e} at {url}")
+
+print("Number of Documents in Queue", site_queue.qsize())
+print(f"Time take: {datetime.now() - start_time}")
+print(num_errors)
